57edc8dc5e957f1809f1c451aed64040cad8acaf
Kimaru Thagana <thagana44@gmail.com>
2017-11-06 10:42:24 +0000

    clean up

--- Client/urls.py
+++ Client/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from graphene_django.views import GraphQLView
+from . import views
 urlpatterns = [
     url(r'^api/$', GraphQLView.as_view(graphiql=True)),
+    url(r'^$',views.home,name="home"),
 ]

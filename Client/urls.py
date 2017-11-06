40263f1b70cefe03843346ad53399d8cc70ee69d
Kimaru Thagana <thagana44@gmail.com>
2017-11-06 11:12:24 +0000

    update client urls.py

--- Client/urls.py
+++ Client/urls.py
@@ -18,5 +18,5 @@ from graphene_django.views import GraphQLView
 from . import views
 urlpatterns = [
     url(r'^api/$', GraphQLView.as_view(graphiql=True)),
-    url(r'^$',views.home,name="home"),
+    
 ]
